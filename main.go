24aa45771260120b92daa38a926e8dbec141b632
septianhari <aribarno86@gmail.com>
2024-09-18 23:15:21 +0700

    add code

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import "fmt"
+
+func main() {
+	todos := Todos{}
+	todos.add("Buy milk")
+	todos.add("Buy eggs")
+	fmt.Printf("%+v\n\n", todos)
+
+	todos.delete(0)
+
+	fmt.Printf("%+v", todos)
+}
--- todo.go
+++ todo.go
@@ -3,11 +3,15 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/aquasecurity/table"
 )
 
 type Todo struct {
-	Tittle      string
+	Title       string
 	Completed   bool
 	CreatedAt   time.Time
 	CompletedAt *time.Time
@@ -15,33 +19,87 @@ type Todo struct {
 
 type Todos []Todo
 
-func (todos *Todos) add(tittle string) {
+func (todos *Todos) add(title string) {
 	todo := Todo{
-		Tittle:      tittle,
+		Title:       title,
 		Completed:   false,
-		CreatedAt:   time.Now(),
 		CompletedAt: nil,
+		CreatedAt:   time.Now(),
 	}
 
 	*todos = append(*todos, todo)
 }
 
-func (todos *Todos) validateIndex(index int) {
+func (todos *Todos) validateIndex(index int) error {
 	if index < 0 || index >= len(*todos) {
-		err := errors.New("Invalid index")
+		err := errors.New("invalid index")
 		fmt.Println(err)
 		return err
 	}
+
 	return nil
 }
 
 func (todos *Todos) delete(index int) error {
-	t := (*todos)[index]
+	t := *todos
 
 	if err := t.validateIndex(index); err != nil {
 		return err
 	}
 
 	*todos = append(t[:index], t[index+1:]...)
+
 	return nil
+}
+
+func (todos *Todos) toggle(index int) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	isCompleted := t[index].Completed
+
+	if !isCompleted {
+		completionTime := time.Now()
+		t[index].CompletedAt = &completionTime
+	}
+
+	t[index].Completed = !isCompleted
+
+	return nil
+}
+
+func (todos *Todos) edit(index int, title string) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	t[index].Title = title
+
+	return nil
+}
+
+func (todos *Todos) print() {
+	table := table.New(os.Stdout)
+	table.SetRowLines(false)
+	table.SetHeaders("#", "Title", "Completed", "Created At", "Completed At")
+	for index, t := range *todos {
+		completed := "❌"
+		completedAt := ""
+
+		if t.Completed {
+			completed = "✅"
+			if t.CompletedAt != nil {
+				completedAt = t.CompletedAt.Format(time.RFC1123)
+			}
+		}
+
+		table.AddRow(strconv.Itoa(index), t.Title, completed, t.CreatedAt.Format(time.RFC1123), completedAt)
+	}
+
+	table.Render()
 }
